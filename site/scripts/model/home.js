f8842565f3a0c7c3d6e606cbc59594df2f146703
jvmqueue <wheels0090@yahoo.com>
2015-09-12 17:01:41 -0400

    define sync. Place Model in private scope. getInstance forms Model as singleton, can be instantiated once only

--- site/scripts/model/home.js
+++ site/scripts/model/home.js
@@ -1,18 +1,42 @@
 define(['jQuery', 'Backbone'], function($, Backbone, undefined){
   
-
-  var _fnc = {
-    Model:Backbone.Model.extend({
+    var Model = Backbone.Model.extend({ // private
       defaults:{
         author:'Halle',
         title:'Barry',
-        url:'https://www.youtube.com'
+        url:'https://www.youtube.com',
+        data:null
+      },
+      initialize:function(){
+
+      },
+      sync:function(param){
+        for(var name in arguments){
+          console.group('SYNC');
+            console.log('name:\t', name);
+            console.log('arguments[name]:\t', arguments[name]);
+           console.groupEnd(); 
+        }
       }
-    })  
-  };
+      /*TODO: GET board.xml*/
+    }); // End Model = Backbone.Model.extend    
+
+  var _fnc = {
+    instance:null,
+    getInstance:function(){ // defines Model as a singleton
+      if(!!_fnc.instance){
+        console.group('GET INSTANCE');
+          console.log(':\t', 'Is instantiated');
+         console.groupEnd(); 
+        return _fnc.instance;
+      }else{
+        _fnc.instance = new Model();        
+      }
+      return _fnc.instance;
+    }
+  }; // End _fnc
 
   return{fnc:_fnc};
-
 
 }); // End require(['jQuery', 'Backbone'] ...
 
