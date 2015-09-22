b903c6a21c24fa6a7be49447bb5beb94cb8f3f44
jvmqueue <wheels0090@yahoo.com>
2015-09-22 16:13:52 -0400

    refactor fragment creation

--- site/scripts/exception/exception.js
+++ site/scripts/exception/exception.js
@@ -1,16 +1,20 @@
 define(['jQuery'], function($, undefined){
     var w = window, d = document;
     var _fnc = {
-        http:function(options){
-            console.log('options.exception:\t', options.exception);
+        _createDocumentFragment:function(paramStrInnerHtml){
             var frag = d.createDocumentFragment();
             var nodeNew = d.createElement('div');
-            var nodeExist = d.getElementsByTagName('body')[0];
-            var strException = options.exception.responseText;
+            var strException = paramStrInnerHtml;
 
             nodeNew.setAttribute('class', 'exception');
             nodeNew.innerHTML = strException;
             frag.appendChild(nodeNew);
+            return frag;
+        },
+        http:function(options){
+            var nodeExist = d.getElementsByTagName('body')[0];
+            var strException = options.exception.responseText;
+            var frag = _fnc._createDocumentFragment(strException);
             nodeExist.appendChild(frag);
         }
     };
