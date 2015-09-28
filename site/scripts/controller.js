2efc27982fd163a6ddc9ba9c4b8d893aa8267443
jvmqueue <wheels0090@yahoo.com>
2015-09-28 17:28:31 -0400

    remove TODO comment

--- site/scripts/controller.js
+++ site/scripts/controller.js
@@ -14,7 +14,7 @@ require(['jQuery', 'Backbone', 'homeModel', 'homeView', 'util', 'exception'],
   var interval = w.setInterval(function(){ // we don't need jQuery to wait for DOM
     if(d.getElementsByTagName('div').length > 1){
       w.clearInterval(interval);
-      var view = new homeView.View(); // TODO: refactor main.js - - > mainController.js then define homeView.js
+      var view = new homeView.View();
     }
   }, 33);
   
