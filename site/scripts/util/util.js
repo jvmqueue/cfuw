ae81d18659306fea01d5e6e79a80d31eaaebd695
jvmqueue <wheels0090@yahoo.com>
2015-10-08 16:28:47 -0400

    adjust for reading XML tag names

--- site/scripts/util/util.js
+++ site/scripts/util/util.js
@@ -1,48 +1,67 @@
-define(['jQuery'], function($undefined){
-    
+define(['jQuery'], function($, undefined){
+
+    var w = window, d = document;    
     var TAG_NAME_DOC_FIRST_CHILD = 'page'; // private member
     var TAG_NAME_DOC_TITLE = 'title'; // private member
+    
 
     var _fnc = {
         noCache:function(){
             return new Date().getMilliseconds();
         },
-        getElementNode:function(paramNode, paramArray){
+        getElementNode:function(paramNode, paramArray, paramIndex){
             var node = paramNode;
+            
             var ELEMENT = 1;
             var arry = paramArray;
             var strNameTitle = '';
             var strValueTitle = '';
             var strName = '';
-            var strValue = '';
-            if(node.firstChild === ELEMENT){
-                console.group('UTIL GET ELEMENT NODE');
-                    console.log('node.firstChild:\t', node.firstChild);
-                   console.groupEnd(); 
-            }else{
-                strNameTitle = node.childNodes[0].nextElementSibling.nodeName;
-                strValueTitle = node.childNodes[0].nextElementSibling.firstChild.nodeValue;
-                strName = node.childNodes[1].nextElementSibling.nodeName;
-                strValue = node.childNodes[1].nextElementSibling.firstChild.nodeValue;
-                arry.push({strNameTitle:strValueTitle, strName:strValue});
+            var strNodeName = '';
+            var strValue = '';       
+
+            if(node.childNodes.length > 0){
+                   paramArray.push(node.childNodes);
             }
         },
         parseXmlToJson:function(paramXml, paramOptionTags){
             var $xmlDoc = $(paramXml);
             var arry = [];
-            var selector = TAG_NAME_DOC_FIRST_CHILD + ' ' + TAG_NAME_DOC_TITLE;
-            var title = $xmlDoc.find(selector)[0].innerHTML;
+            var arryElementNodes = [];
+            var hashElementNodes = {};
+            var selectorPageTitle = TAG_NAME_DOC_FIRST_CHILD + ' ' + TAG_NAME_DOC_TITLE;
+            var title = $xmlDoc.find(selectorPageTitle)[0].innerHTML;
             var tagNameContainer = paramOptionTags.childContainerTag;
             var tagNameFirstChild = paramOptionTags.firstChildTag;
-            selector = tagNameContainer + ' ' + tagNameFirstChild;
+            var selector = tagNameContainer + ' ' + tagNameFirstChild;
             var members = $xmlDoc.find(selector);          
             var nodeTagName = '';
             var nodeValue = ''; 
 
             for(var i = 0, len = members.length; i < len; i++){
-                var node = _fnc.getElementNode(members[i], arry);
+                arry.push(members[i]);
             }
-            return {pageTitle:title, pageData:arry};
+
+            for(var i = 0, len = arry.length; i < len; i++){
+                
+                    $(arry[i].childNodes).each(function(index, elm){
+
+                        if(this.nodeType == 1){
+                           nodeValue = elm.firstChild.nodeValue; 
+                           nodeTagName = elm.nodeName;
+
+                           hashElementNodes = {'strTagName':nodeTagName, 'strName':nodeValue, 'strNameTitle':'Util Fix Title'};
+                           arryElementNodes.push(hashElementNodes);                         
+                        }
+                    }); 
+                
+            } // End for
+
+                           console.group('PARSE XML TO JSON');
+                            console.log('arryElementNodes:\t', arryElementNodes);
+                           console.groupEnd();               
+
+            return {pageTitle:title, pageData:arryElementNodes};
         } // End parseXmlToJson
     };
     return{
