6fe575eedda1b706ca26f7ebdf6927ae72c732a4
jvmqueue <wheels0090@yahoo.com>
2015-10-13 18:42:29 -0400

    not the most elegant, but performing research on requireJs returned mixed results

---
+++ site/scripts/basePath.js
@@ -0,0 +1,7 @@
+define([], function(){
+    var strBasePath = '../CFUW_Malaika/data/';
+    var _basePath = function(){
+        return strBasePath;    
+    };
+    return {basePath:_basePath}
+});
