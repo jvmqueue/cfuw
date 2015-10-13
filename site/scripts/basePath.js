1eefc407fa974dd3851160946378a2202d2b6ca8
jvmqueue <wheels0090@yahoo.com>
2015-10-13 19:07:53 -0400

    local repository failure

--- site/scripts/basePath.js
+++
@@ -1,7 +0,0 @@
-define([], function(){
-    var strBasePath = '../CFUW_Malaika/data/';
-    var _basePath = function(){
-        return strBasePath;    
-    };
-    return {basePath:_basePath}
-});
