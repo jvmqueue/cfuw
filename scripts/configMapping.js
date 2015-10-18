722638b87954bd8356a6b6e7f045ee0cebdee91e
jvmqueue <wheels0090@yahoo.com>
2015-10-18 14:24:45 -0400

    HTML click event mapping to XML data

---
+++ scripts/configMapping.js
@@ -0,0 +1,22 @@
+define([], function(undefined){
+
+    var _mapping = {
+        viewToData:{
+            HOME:{control:'btnHome', data:'home.xml'},
+            MISSION_STATEMENT:{control:'btnMissionStatement', data:'missionStatement.xml'},
+            CONTACT_US:{control:'btnContactUs', data:'contactUs.xml'},
+            BOARD:{control:'btnBoadMembers', data:'board.xml'},
+            AFFILIATIONS:{control:'btnAffiliations', data:'affiliations.xml'},
+            MEMBERSHIP_APPLICATION:{control:'btnMembershipApplication', data:'membershipApplication.xml'}
+        }
+    };
+
+    return{
+        mapping:_mapping
+    }
+
+}); // End define([] ...
+
+
+
+
