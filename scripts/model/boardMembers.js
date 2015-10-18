4ea8ad0bfcec4320a917a304352ed558db182522
jvmqueue <wheels0090@yahoo.com>
2015-10-18 14:23:52 -0400

    new model for boardMembers

---
+++ scripts/model/boardMembers.js
@@ -0,0 +1,42 @@
+define(['jQuery', 'Backbone', 'util', 'basePath'], function($, Backbone, util, base){
+    
+    var Model = Backbone.Model.extend({ // private
+      defaults:{
+        url:base.basePath() + 'board.xml',
+        idAttribute:'boardId',
+        arryTemplateData:'',
+        cid:'boardId',
+        data:null
+      },
+      parse:function(paramXmlResponse){
+        var xmlResponse = paramXmlResponse;   
+        var hash = util.fnc.parseXmlToJson(xmlResponse, {childContainerTag:'members', firstChildTag:'member'});
+        this.set('arryTemplateData', hash.pageData);                 
+        this.set('pageTitle', hash.pageTitle);
+      },
+      initialize:function(){
+
+      }
+
+    }); // End Model = Backbone.Model.extend    
+
+  var _fnc = {
+    instance:null,
+    getInstance:function(options){ // defines Model as a singleton
+      var strModelId = options.modelId;
+      if(!!_fnc.instance){
+        return _fnc.instance;
+      }else{
+        _fnc.instance = new Model({id:strModelId});
+      }
+      return _fnc.instance;
+    }
+  }; // End _fnc
+
+  return{fnc:_fnc};
+
+}); // End require(['jQuery', 'Backbone'] ...
+
+
+
+
