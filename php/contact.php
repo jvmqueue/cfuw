e3338b3e53ced276c242325d45c5125c1da822a0
jvmqueue <wheels0090@yahoo.com>
2016-01-25 11:04:27 -0500

    get_header now only gets form values. Removed concatenation. mail function works now without sending reply-to header. Gmail does not acknowlege emails having decimals in name

--- php/contact.php
+++ php/contact.php
@@ -2,31 +2,32 @@
 
 // REPLACE THE LINE BELOW WITH YOUR E-MAIL ADDRESS.
 $to = 'developer@cfuw-saskatoon.org';
-$subject = 'Contact Us: Canadian Federation University of Women';
+$mSubject = 'CFUW Contact Us:' . "\t";
 
 function get_header(){
     
     $arrayCompleteMessage = array();
     $strName = '';
-    global $subject; // access global
-    $arrayCompleteMessage['subject'] = $subject;
 
     foreach (getallheaders() as $name => $value){
         
         $strName = strtolower($name);
 
         switch($strName){
+            case 'contact-subject':   
+                $arrayCompleteMessage['subject'] = $value;
+                break;    
             case 'contact-first':   
-                $arrayCompleteMessage['first'] = "First:\t" . $value;
+                $arrayCompleteMessage['first'] =  $value;
                 break;            
             case 'contact-last': 
-                $arrayCompleteMessage['last'] = "Last:\t" . $value;
+                $arrayCompleteMessage['last'] =  $value;
                 break;
             case 'contact-email': 
-                $arrayCompleteMessage['email'] = "Email:\t" . $value;
+                $arrayCompleteMessage['email'] = $value;
                 break;
             case 'contact-message': 
-                $arrayCompleteMessage['message'] = "Message:\t" . $value;
+                $arrayCompleteMessage['message'] = $value;
                 break;                
             default:
                 // do nothing discovered header name we are not tracking                
@@ -38,25 +39,31 @@ function get_header(){
 }
 $newLine = "\r\n";
 $arrayMessage = get_header();
+// not using this, because email goes to spam
+// $headerFrom = "From: <" . $arrayMessage['email'] . ">";
 
-$headerFrom = "From: <" . $arrayMessage['email'] . ">"; // not using this, because email goes to spam
-$emailSubject = $arrayMessage['subject'];
+// Bug: Gmail does not accept emails having special characters
+// $headerSender = "Reply-To: <" . $strEmail . ">" . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+
+/*Works providing no decimal in name*/
+// $headerSender = "Reply-To: $strEmail" . "\r\n" . 'X-Mailer: PHP/' . phpversion();
+
+$strEmail = trim($arrayMessage['email']);
+$emailSubject = $mSubject . $arrayMessage['subject'];
 
 /*Perform concatenation manually, because order is important*/
-// TODO: replace first space in email with @. And second space with .
-$strMessage = $arrayMessage['subject'] . $newLine;
-$strMessage .= $arrayMessage['first'] . $newLine;
-$strMessage .= $arrayMessage['last'] . $newLine;
-$strMessage .= $arrayMessage['email'] . $newLine;
-$strMessage .= $arrayMessage['message'];
+$strMessage = $mSubject . $arrayMessage['subject'] . $newLine;
+$strMessage .= "First:\t" . $arrayMessage['first'] . $newLine;
+$strMessage .= "Last:\t" . $arrayMessage['last'] . $newLine;
+$strMessage .= "Email:\t" . $arrayMessage['email'] . $newLine;
+$strMessage .= "Message:\r\n" . $arrayMessage['message'];
 
 $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 fwrite($myfile, $strMessage);
-fwrite($myfile, "\r\nstrMessageStripped:");
-fwrite($myfile, $strMessageStripped);
+fwrite($myfile, $newLine. 'headerSender::::'. $headerSender);
+fwrite($myfile, "\r\nstrEmail trimmed::::" . $strEmail);
 fclose($myfile);                 
 
 mail($to, $emailSubject, $strMessage);
-
 
 ?>
