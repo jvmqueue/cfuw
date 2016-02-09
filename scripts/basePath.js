fbc952d1d659da5ff474d2f0c6dca116eeef301f
jvmqueue <wheels0090@yahoo.com>
2016-02-09 08:54:18 -0500

    optimization... Assume in dev environment. If local, reassign paths

--- scripts/basePath.js
+++ scripts/basePath.js
@@ -4,17 +4,15 @@ define([], function(){
         dev:{working:'../CFUW_Publisher/data/', fallback:'data/'}
     };
     var strHostName = window.location.hostname;
-    var strWorking = null;
-    var strFallBack = null;
+    var strWorking = strBasePath.dev.working;
+    var strFallBack = strBasePath.dev.fallback;
     var _basePath = null;
 
     if(strHostName === '127.0.0.1'){
         strWorking = strBasePath.local.working
         strFallBack = strBasePath.local.fallback
-    }else{
-        strWorking = strBasePath.dev.working
-        strFallBack = strBasePath.dev.fallback            
     }
+    
     _basePath = {working:strWorking, fallback:strFallBack};
 
     return {dir:_basePath}
